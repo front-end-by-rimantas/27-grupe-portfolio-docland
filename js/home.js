8bacaa10425735a76bdfd92b512aaa104ed35bea
MarijusIL <78425010+MarijusIL@users.noreply.github.com>
2021-05-26 18:37:27 +0300

    added comments to home.js for import and execution

--- js/home.js
+++ js/home.js
@@ -1,3 +1,51 @@
 // IMPORT
 
+// header import start
+// header import end
+
+// hero import start
+// hero import end
+
+// counter import start
+// counter import end
+
+// what-you-learn import start
+// what-you-learn import end
+
+// syllabus import start
+// syllabus import end
+
+// benefit import start
+// benefit import end
+
+// reviews import start
+// reviews import end
+
+// enrol form import start
+// enrol form import end
+
 // EXECUTION
+
+// header execution start
+// header execution end
+
+// hero execution start
+// hero execution end
+
+// counter execution start
+// counter execution end
+
+// what-you-learn execution start
+// what-you-learn execution end
+
+// syllabus execution start
+// syllabus execution end
+
+// benefit execution start
+// benefit execution end
+
+// reviews execution start
+// reviews execution end
+
+// enrol form execution start
+// enrol form execution end
