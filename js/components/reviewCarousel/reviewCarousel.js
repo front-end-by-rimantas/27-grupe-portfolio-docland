8b89a84784a7c6a09fca8a02b11f91572e4fc9c8
RomanSaltis <roman.saltis@gmail.com>
2021-06-05 12:16:52 +0300

    Rendering carousel

--- js/components/reviewCarousel/reviewCarousel.js
+++ js/components/reviewCarousel/reviewCarousel.js
@@ -1,4 +1,41 @@
 class reviewCarousel {
+    constructor(selector, component, data) {
+        this.selector = selector;
+        this.component = component;
+        this.data = data;
+
+        this.DOM = null;
+        this.init();
+    }
+
+    init() {
+        if (!this.isValidSelector()) {
+            return false;
+        }
+
+        this.render();
+
+    }
+
+    isValidSelector() {
+        if (typeof this.selector !== 'string' ||
+            this.selector === '') {
+            return false;
+        }
+        return true;
+    }
+
+    findElementBySelector() {
+        this.DOM = document.querySelector(this.selector);
+        if (!this.DOM) {
+            return false;
+        }
+        return true;
+    }
+
+    render() {
+        console.log('Rendering carousel...');
+    }
 
 }
 
--- js/data/reviewCarouselData.js
+++ js/data/reviewCarouselData.js
@@ -1,12 +1,21 @@
 const reviewCarouselData = {
     imgPath: './img/review/',
     itrmsPerView: [
-        {},
-        {},
-        {},
-        {},
+        {
+            minWidth: 0,
+            itemCount: 1
+        },
+        {
+            minWidth: 500,
+            itemCount: 2
+        },
+
     ],
-    list: []
+    list: [
+        {},
+        {},
+        {},
+    ]
 }
 
 export { reviewCarouselData }
