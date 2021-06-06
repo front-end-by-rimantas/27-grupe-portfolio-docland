53226de0cf5b99dcab90ff25d53342a4973cba54
RomanSaltis <roman.saltis@gmail.com>
2021-06-06 23:33:08 +0300

    import html

--- js/components/reviewCarousel/reviewCarousel.js
+++ js/components/reviewCarousel/reviewCarousel.js
@@ -34,12 +34,40 @@ class reviewCarousel {
     }
 
     render() {
-        const HTML = <div class="reviews container">
-            <div class="row">
-
+        const HTML = `<div class="reviews carousel">
+            <div class="reviews controls">
+                <div class="arrows">
+                    <h2 class="reviews-title">Student Reviews</h2>
+                </div>
+                <div class="arrows">
+                    <div class="arrow"></div>
+                    <div class="arrow"></div>
+                </div>
             </div>
+            <div id="review_block" class="reviews galery grid-container">
+                <div class="first">Review</div>
+                <div class="second">Review</div>
+                <div class="third">Review</div>
+            </div>
+            <div class="reviews stat">
+                <div class="ratings">
+                    <h2 class="average">4.85/5</h2>
+                    <span>Average Ratings</span>
+                </div>
+                <div class="ratings">
+                    <h2 class="student">15.4k</h2>
+                    <span>Student Ratings</span>
+                </div>
+            </div>
+            <div class="all">
+                <div class="all reviews">
+                    <a href="#">View All Reviews</a>
+                </div>
+            </div>
+        </div>`;
 
-        </div>
+        this.DOM.innerHTML = HTML;
+
     }
 
 }
