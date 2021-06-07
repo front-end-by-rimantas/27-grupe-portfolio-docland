61a20ce5e98bf67801b3d7d1a140834aabcee7b7
ssinare <82803125+ssinare@users.noreply.github.com>
2021-06-07 12:51:50 +0300

    js header try#1

--- js/components/header/Header.js
+++ js/components/header/Header.js
@@ -1,11 +1,30 @@
 function header(selector, data) {
     const hamburger = document.querySelector('.hamburger')
+    const close = document.querySelector('.verticalMeniuClose')
+    let isSidebarVisible = false;
+    hamburger.addEventListener('click', () => {
+        verticalMeniu(isVisisble)
+        isVisible = !isVisible;
+        hamburger.style.display = '';
+        close.style.display = 'block';
+    })
+
+    close.addEventListener('click', () => {
+        verticalMeniu(isVisisble)
+        isVisible = !isVisible;
+        hamburger.style.display = 'block';
+        close.style.display = 'none';
+    })
     //validation
-
-
     //logic
 }
+function verticalMeniu(isVisible) {
+    constheaderVerticalMeniu = document.getElementsByClassName('vertical-meniu');
+    if (isVisible) {
 
+    }
+
+}
 
 
 
