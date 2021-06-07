9d7d392383f2ac32a5684401ec6c37a4566ec421
MarijusIL <78425010+MarijusIL@users.noreply.github.com>
2021-06-07 07:15:34 +0300

    added syllabus data

---
+++ js/data/syllabusData.js
@@ -0,0 +1,162 @@
+const syllabusData = {
+    week1: [
+        {
+            title: 'Welcome and introduction: About Course',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'Introduction about UX Design',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'The Difference between UI and UX?',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'Talking about UX industry',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'How to apply UX on our daily project',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'What will I be making as a UX Designer?',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+    ],
+    week2: [
+        {
+            title: 'Welcome and introduction: About Course',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'Introduction about UX Design',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'The Difference between UI and UX?',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'Talking about UX industry',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'How to apply UX on our daily project',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'What will I be making as a UX Designer?',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+    ],
+    week3: [
+        {
+            title: 'Welcome and introduction: About Course',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'Introduction about UX Design',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'The Difference between UI and UX?',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'Talking about UX industry',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'How to apply UX on our daily project',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'What will I be making as a UX Designer?',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+    ],
+    week4: [
+        {
+            title: 'Welcome and introduction: About Course',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'Introduction about UX Design',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'The Difference between UI and UX?',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'Talking about UX industry',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'How to apply UX on our daily project',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'What will I be making as a UX Designer?',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+    ],
+    week5: [
+        {
+            title: 'Welcome and introduction: About Course',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'Introduction about UX Design',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'The Difference between UI and UX?',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'Talking about UX industry',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'How to apply UX on our daily project',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+        {
+            title: 'What will I be making as a UX Designer?',
+            duration: '03:29',
+            videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        },
+    ]
+}
