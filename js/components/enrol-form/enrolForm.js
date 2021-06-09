a3a6de30ba9100fd390e5d359c05bd570cf50fc8
ssinare <82803125+ssinare@users.noreply.github.com>
2021-06-09 22:51:58 +0300

    Statistic styling

--- js/components/enrol-form/enrolForm.js
+++ js/components/enrol-form/enrolForm.js
@@ -1,15 +1,13 @@
 function progressLine() {
     const emailInputField = document.querySelector('.input');
     const emailLine = document.getElementById('progressLine');
-    const arrow =
-        emailInputField.addEventListener('click', () => {
-            console.log("Pavyko");
-            emailLine.style.animationDuration = "20s";
-            emailLine.style.backgroundColor = '#fbc006';
-        })
+
+    emailInputField.addEventListener('click', () => {
+        emailLine.style.animationDuration = "20s";
+        emailLine.style.backgroundColor = '#fbc006';
+    })
 
     emailInputField.addEventListener('focusout', () => {
-        console.log("Blur");
         emailLine.style.backgroundColor = '#fff';
     })
 }
